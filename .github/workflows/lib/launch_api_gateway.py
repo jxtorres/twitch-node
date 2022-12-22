39d163a9e5f34b80f4ebad1c63e7fe6c9c9f4438
Jordan <jxtbronco@gmail.com>
2022-12-21 22:40:23 -0600

    mod api gateway launcher

--- .github/workflows/lib/launch_api_gateway.py
+++ .github/workflows/lib/launch_api_gateway.py
@@ -8,70 +8,45 @@ def tryLaunchGateway(functionName, accountId, region):
     # Create an AWS client for the API Gateway service
     client = boto3.client('apigateway')
 
-
     # Get the list of REST APIs in the account
     response = client.get_rest_apis()
 
     # Iterate through the list of REST APIs
     for api in response['items']:
         # Check if the API has the name we are looking for
-        if api['name'] == 'My API':
+        if api['name'] == 'MyAPI':
             # Return the ID of the matching REST API
             print("API Gateway API already created.")
             return
 
     print("Proceeding with API Gateway launch")
-
-    # Create the REST API
-    response = client.create_rest_api(
-        name='My API',
-        description='This is my API'
+    
+    # Create a new REST API
+    response = apigateway.create_rest_api(
+        name='MyAPI',
+        description='This is my API',
+        version='2020-01-01'
     )
-
-    # Get the ID of the newly created REST API
     api_id = response['id']
 
-
-    print("my new api id..", api_id)
-
-
-    # Get the list of REST APIs in the account
-    response = client.get_rest_apis()
-
-    # Iterate through the list of REST APIs
-    for api in response['items']:
-        # Print the ID of the REST API
-        print(api['id'])
-
-    time.sleep(15)
-
-    # Create a resource in the REST API
-    response = client.create_resource(
+    # Create a GET method on the root resource
+    response = apigateway.put_method(
         restApiId=api_id,
-        parentId=api_id,
-        pathPart='myresource'
-    )
-
-    # Get the ID of the newly created resource
-    resource_id = response['id']
-
-    # Create a method for the resource
-    response = client.put_method(
-        restApiId=api_id,
-        resourceId=resource_id,
+        resourceId='/',
         httpMethod='GET',
         authorizationType='NONE'
     )
 
-    # Create an integration for the method
-    response = client.put_integration(
+    # Create a Lambda function integration
+    response = apigateway.put_integration(
         restApiId=api_id,
-        resourceId=resource_id,
+        resourceId='/',
         httpMethod='GET',
         type='AWS_PROXY',
         integrationHttpMethod='POST',
         uri=f"arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/arn:aws:lambda:{region}:{accountId}:function:{functionName}/invocations"
     )
+
 
     #Deploy the REST API
     response = client.create_deployment(
