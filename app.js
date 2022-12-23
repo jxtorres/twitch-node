5d2eb17e5b3ed5d330540abf51b033bd7f883a1c
Jordan <jxtbronco@gmail.com>
2022-12-22 20:58:43 -0600

    fixing callback usage for lambda

--- app.js
+++ app.js
@@ -6,4 +6,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-module.exports.handler = app;
+exports.handler = (event, context, callback) => {
+  // Create an HTTP request and response object from the event and context
+  const req = event.httpMethod ? event : {
+    httpMethod: event.method,
+    headers: event.headers,
+    queryStringParameters: event.queryStringParameters,
+    path: event.path,
+    body: event.body
+  };
+  const res = {
+    setHeader: (name, value) => {
+      context.headers[name] = value;
+    },
+    send: (body) => {
+      context.body = body;
+    }
+  };
+
+  // Invoke the express app with the request and response objects
+  app(req, res, (err) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, {
+      statusCode: context.statusCode,
+      headers: context.headers,
+      body: context.body
+    });
+  });
+};
