2f6b84cfdb34614569858db703a6a95bd77a2712
Jordan <jxtbronco@gmail.com>
2022-12-22 19:00:51 -0600

    mod layer

--- .github/workflows/lib/deploy.py
+++ .github/workflows/lib/deploy.py
@@ -9,19 +9,11 @@ def update_lambda_function(function_name, source_zip_file, lib_zip_file, runtime
 
     # If the function does not exist, create it
     if not function_exists:
-        with open(source_zip_file, 'rb') as f:
-            code = f.read()
-            client.create_function(
-                FunctionName=function_name,
-                Runtime=runtime,
-                Role='arn:aws:iam::682749323867:role/lambda-twitch-node',
-                Handler=handler,
-                Code={'ZipFile': code}
-            )
+
         with open(lib_zip_file, 'rb') as f:
             libs = f.read()
 
-            client.publish_layer_version(
+            response = client.publish_layer_version(
                 LayerName='my-layer',
                 Description='My layer',
                 CompatibleRuntimes=[runtime],
@@ -36,6 +28,18 @@ def update_lambda_function(function_name, source_zip_file, lib_zip_file, runtime
             #     Handler=handler,
             #     Code={'ZipFile': libs}
             # )
+
+        with open(source_zip_file, 'rb') as f:
+            code = f.read()
+            client.create_function(
+                FunctionName=function_name,
+                Runtime=runtime,
+                Role='arn:aws:iam::682749323867:role/lambda-twitch-node',
+                Handler=handler,
+                Code={'ZipFile': code},
+                Layers=[response['LayerVersionArn']]
+            )
+        
     # If the function exists, update the code
     else:
         with open(zip_file, 'rb') as f:
